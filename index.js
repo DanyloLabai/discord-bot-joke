8139d98c1b8f3a054b59618a988a2db94e6d8040
DanyloLabai <danylolabai@gmail.com>
2025-05-30 16:52:00 +0300

    add models

---
+++ index.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import { Client, GatewayIntentBits } from 'discord.js';
+import fetch from 'node-fetch';
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ]
+});
+
+
+client.once('ready', () => {
+    console.log(`Bot logged in as ${client.user.tag}`);
+})
+
+client.on('messageCreate', async message => {
+    if(message.author.bot) return;
+
+    if(message.content.toLowerCase() === '!joke'){
+        try{
+            const res = await fetch(`http://localhost:3000/api/jokes/random`);
+            const data = await res.json();
+            message.channel.send(DataTransfer.text);
+        }catch(err){
+            message.channel.send('Error');
+        }
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
